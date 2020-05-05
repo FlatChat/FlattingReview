0c9f158a81f597508ef3bf71294beb3e3fdb6bcd
Ryan <racole1998@gmail.com>
2020-05-05 20:29:39 +1200

    Completed a basic layout for the three screens: flat, viewReview and WriteREview

--- app/src/main/java/com/example/flattingreview/WriteReview.kt
+++ app/src/main/java/com/example/flattingreview/WriteReview.kt
@@ -1,16 +1,12 @@
 package com.example.flattingreview
 
-import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-
+class WriteReview : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_write_review)
     }
 }
