a3658c1f6d683694b9f348802a7bd8bd26b73cf3
Ryan <racole1998@gmail.com>
2020-05-14 14:57:36 +1200

    Write review ready to write to database

--- app/src/main/java/com/example/flattingreview/WriteReview.kt
+++ app/src/main/java/com/example/flattingreview/WriteReview.kt
@@ -5,19 +5,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+// Write a message to the database
+private val database = Firebase.database
+private val myRef = database.getReference("message")
 
 class WriteReview : AppCompatActivity() {
-
-//    // Get a reference to the database service
-////    var database = firebase.database();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_review)
+
+        // Get reference to button
+        val buttonClick = findViewById<Button>(R.id.submit_button)
+        // Get reference to the text
+        val comment = findViewById<EditText>(R.id.comment)
+        // Get reference to the rating bars
+        val cleanliness = findViewById<RatingBar>(R.id.cleanlinessRatingBar)
+        val landlord = findViewById<RatingBar>(R.id.landlordRatingBar)
+        val location = findViewById<RatingBar>(R.id.locationRatingBar)
+        val value = findViewById<RatingBar>(R.id.valueRatingBar)
+
+        // Button Listener will save the information into the database
+        // when the submit button is clicked.
+        buttonClick.setOnClickListener {
+            myRef.setValue(comment)
+            myRef.setValue(cleanliness)
+            myRef.setValue(landlord)
+            myRef.setValue(location)
+            myRef.setValue(value)
+            //myRef.setValue(userID)
+        }
     }
-
-
-
 
     //below code is all for the action bar
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
