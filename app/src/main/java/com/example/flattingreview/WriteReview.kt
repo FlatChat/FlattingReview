fa7dadb1f7530236767bb48475e2154a949fe5b5
Ryan <racole1998@gmail.com>
2020-05-14 16:29:49 +1200

    Added Comments

--- app/src/main/java/com/example/flattingreview/WriteReview.kt
+++ app/src/main/java/com/example/flattingreview/WriteReview.kt
@@ -32,6 +32,7 @@ class WriteReview : AppCompatActivity() {
         val value = findViewById<RatingBar>(R.id.valueRatingBar).rating
         val anon = findViewById<Switch>(R.id.anonSwitch).isChecked
 
+        // Set at 0 until we have the working accounts
         val flatID = 0
         val userID = 0
         val reviewID = 0
