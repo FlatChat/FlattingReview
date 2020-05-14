fb2c765f7364c29d7676afc770b1b2d5c98ee3a1
nikki <nikkimeadows1@gmail.com>
2020-05-14 13:27:06 +1200

    added a button to take the user to the create a new flat screen.

---
+++ app/src/main/java/com/example/flattingreview/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.flattingreview
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //connecting the splash screen dummy button to the sign in screen
+       splashScreenButton.setOnClickListener{
+            val intent = Intent(this,SignIn::class.java)
+           startActivity(intent)
+       }
+
+    }
+}
