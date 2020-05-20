246376365d26491d346740513d674bc02f95e84d
nikki <nikkimeadows1@gmail.com>
2020-05-21 09:17:51 +1200

    Changed logout toolbar to Settings and started working on letting the user change their password function.

--- app/src/main/java/com/example/flattingreview/Settings.kt
+++ app/src/main/java/com/example/flattingreview/Settings.kt
@@ -1,7 +1,11 @@
 package com.example.flattingreview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class Settings : AppCompatActivity() {
 
@@ -9,5 +13,46 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        //connecting the create a flat button to the create a new flat screen
+        changePassBut.setOnClickListener{
+            val intent = Intent(this,ChangePassword::class.java)
+            startActivity(intent)
+        }
+    }
+
+    //below code is all for the action bar
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id=item.itemId
+
+        //If home screen option is pressed go to home screen
+        if(id==R.id.homescreen)
+        {
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        }
+        //If write a review option is pressed go to review screen
+        if(id==R.id.writereview)
+        {
+            val intent = Intent(this, WriteReview::class.java)
+            startActivity(intent)
+        }
+        //If contact us option is pressed go to contact us screen
+        if(id==R.id.contact)
+        {
+            val intent = Intent(this, Contact::class.java)
+            startActivity(intent)
+        }
+        //If logout option is selected then redirect user to the login screen
+        if(id==R.id.settings)
+        {
+            val intent = Intent(this, Settings::class.java)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
