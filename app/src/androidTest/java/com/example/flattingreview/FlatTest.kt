483be1fc29463018dd2d437fa53ee10e79de14b8
Ryan <racole1998@gmail.com>
2020-08-16 09:28:39 +1200

    Added FlatTest.kt

---
+++ app/src/androidTest/java/com/example/flattingreview/FlatTest.kt
@@ -0,0 +1,54 @@
+
+package com.example.flattingreview
+/*
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+*/
+/**
+ *  A class that tests the UI of the change password screen.
+ *
+ *@author Nikki Meadows
+ *//*
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class FlatTest{
+
+    */
+/**
+ *  A test to check that the visibility of the flat screen.
+ *
+ *//*
+
+*/
+/*    @Test
+    fun test_isActivityInView() {
+        val activityScenario=ActivityScenario.launch(FlatScreen::class.java)
+        onView(withId(R.id.flat_screen_id))
+            .check(matches(isDisplayed()))
+    }*//*
+
+    */
+/**
+ *  A test to check that visibility of the buttons on the flat screen.
+ *
+ *//*
+
+    @Test
+    fun test_Visibility_Review_Buttons() {
+        val activityScenario=ActivityScenario.launch(FlatScreen::class.java)
+        onView(withId(R.id.show_reviews_button))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.add_review_button))
+            .check(matches(isDisplayed()))
+    }
+}
+*/
