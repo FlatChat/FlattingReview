ab1772a1732ef2f0b163fc8dae63f1a0697ae001
Ryan <racole1998@gmail.com>
2020-08-15 17:10:51 +1200

    More tests

--- app/src/androidTest/java/com/example/flattingreview/WriteReviewTest.kt
+++ app/src/androidTest/java/com/example/flattingreview/WriteReviewTest.kt
@@ -6,14 +6,12 @@ import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class WriteReviewTest {
 
     @Test
     fun onCreate() {
-//        ActivityScenario.launch(WriteReview::class.java)
-//        Espresso.onView(ViewMatchers.withId(R.id.write_review))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        ActivityScenario.launch(WriteReview::class.java)
+        Espresso.onView(ViewMatchers.withId(R.id.write_review))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
