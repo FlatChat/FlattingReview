427ab261d660f4cdc28907cae8f6ef4bb5563aed
Ryan <racole1998@gmail.com>
2020-08-16 15:41:32 +1200

    Tests

--- app/src/androidTest/java/com/example/flattingreview/WriteReviewTest.kt
+++ app/src/androidTest/java/com/example/flattingreview/WriteReviewTest.kt
@@ -1,17 +1,56 @@
 package com.example.flattingreview
 
+import android.content.Intent
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import models.Flat
 import org.junit.Test
 
+/**
+ * Tests that everything is displayed properly in the write review activity.
+ * @author Ryan
+ */
 class WriteReviewTest {
 
+    /**
+     * Tests that everything is displayed.
+     *
+     */
     @Test
     fun onCreate() {
-//        ActivityScenario.launch(WriteReview::class.java)
-//        Espresso.onView(ViewMatchers.withId(R.id.comment1))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        val flat = Flat("-M348TH", "48 Union St, North Dunedin", "5", "2")
+        val intent = Intent(ApplicationProvider.getApplicationContext(), WriteReview::class.java)
+            .putExtra("flat", flat)
+        val scenario = ActivityScenario.launch<WriteReview>(intent)
+        scenario.onActivity {}
+            Espresso.onView(ViewMatchers.withId(R.id.write_review_address))
+               .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.textView2))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.textView))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.landlord))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.textView3))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.textView4))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.cleanliness))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.location))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.value))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.comment1))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.anonSwitch))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.cancel))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            Espresso.onView(ViewMatchers.withId(R.id.submit_button))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
